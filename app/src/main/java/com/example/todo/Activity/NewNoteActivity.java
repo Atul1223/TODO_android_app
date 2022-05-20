e3496bb72ecbc286ceccf630f885a2889d561b78
Atul1223 <a1atulk@gmail.com>
2022-05-20 23:04:37 +0530

    notification

--- app/src/main/java/com/example/todo/Activity/NewNoteActivity.java
+++ app/src/main/java/com/example/todo/Activity/NewNoteActivity.java
@@ -1,30 +1,44 @@
 package com.example.todo.Activity;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import com.example.todo.services.NotificationPublisher;
+
+import android.app.AlarmManager;
 import android.app.DatePickerDialog;
+import android.app.Notification;
+import android.support.v4.app.INotificationSideChannel;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
-import android.text.InputType;
 import android.view.View;
 import android.widget.DatePicker;
-import android.widget.EditText;
 import android.widget.Toast;
 import com.example.todo.Database.NotesDatabase;
 import com.example.todo.MainActivity;
 import com.example.todo.R;
 import com.example.todo.databinding.ActivityNewNoteBinding;
+import com.example.todo.services.NotificationReceiver;
+
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class NewNoteActivity extends AppCompatActivity {
-
+    public static final String NOTIFICATION_CHANNEL_ID = "10001" ;
     ActivityNewNoteBinding activityNewNoteBinding;
     String title="",subtitle="",note="",priority="",toastString="",deadLine;
     NotesDatabase notesDatabase;
     Intent intent;
+    private final static String default_notification_channel_id = "default" ;
+    final Calendar myCalendar = Calendar. getInstance () ;
 
-
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +51,9 @@ public class NewNoteActivity extends AppCompatActivity {
         toastString="";
         deadLine="";
         notesDatabase=new NotesDatabase(getApplicationContext(),"notes_db",null,1);
+
+
+
 
        activityNewNoteBinding.greenPriority.setOnClickListener(new View.OnClickListener() {
            @Override
@@ -113,5 +130,67 @@ public class NewNoteActivity extends AppCompatActivity {
             }
         });
     }
+//    private void scheduleNotification(Notification notification  , long delay) {
+//        Intent notificationIntent = new Intent( this, NotificationPublisher. class ) ;
+//        notificationIntent.putExtra(NotificationPublisher.NOTIFICATION_ID , 1 ) ;
+//        notificationIntent.putExtra(NotificationPublisher. NOTIFICATION , notification) ;
+//        PendingIntent pendingIntent = PendingIntent. getBroadcast ( this, 0 , notificationIntent , PendingIntent. FLAG_UPDATE_CURRENT ) ;
+//        AlarmManager alarmManager = (AlarmManager) getSystemService(Context. ALARM_SERVICE ) ;
+//        assert alarmManager != null;
+//        alarmManager.set(AlarmManager. ELAPSED_REALTIME_WAKEUP , delay , pendingIntent) ;
+//    }
+//    private Notification getNotification (String content) {
+//        NotificationCompat.Builder builder = new NotificationCompat.Builder( this, default_notification_channel_id ) ;
+//        builder.setContentTitle( "Scheduled Notification" ) ;
+//        builder.setContentText(content) ;
+//        builder.setSmallIcon(R.drawable. ic_launcher_foreground ) ;
+//        builder.setAutoCancel( true ) ;
+//        builder.setChannelId( NOTIFICATION_CHANNEL_ID ) ;
+//        return builder.build() ;
+//    }
+    DatePickerDialog.OnDateSetListener date = (view, year, monthOfYear, dayOfMonth) -> {
+        myCalendar .set(Calendar. YEAR , year) ;
+        myCalendar .set(Calendar. MONTH , monthOfYear) ;
+        myCalendar .set(Calendar. DAY_OF_MONTH , dayOfMonth) ;
+        updateLabel() ;
+    };
+    public void setDate (View view) {
+        new DatePickerDialog(
+                NewNoteActivity. this, date ,
+                myCalendar .get(Calendar. YEAR ) ,
+                myCalendar .get(Calendar. MONTH ) ,
+                myCalendar .get(Calendar. DAY_OF_MONTH )
+        ).show() ;
+
+
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(Calendar.HOUR_OF_DAY, 8);
+            calendar.set(Calendar.MINUTE, 0);
+            calendar.set(Calendar.SECOND, 0);
+            calendar.set(Calendar.YEAR,myCalendar.get(Calendar.YEAR));
+            calendar .set(Calendar. MONTH , myCalendar .get(Calendar. MONTH ) ) ;
+            calendar .set(Calendar. DAY_OF_MONTH , myCalendar .get(Calendar. DAY_OF_MONTH )) ;
+
+            if (calendar.getTime().compareTo(new Date()) < 0)
+                calendar.add(Calendar.DAY_OF_MONTH, 1);
+
+            Intent intent = new Intent(getApplicationContext(), NotificationReceiver.class);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+            AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+
+            if (alarmManager != null) {
+                alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
+
+            }
+
+    }
+    private void updateLabel () {
+        String myFormat = "dd/MM/yy" ; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat , Locale. getDefault ()) ;
+        Date date = myCalendar .getTime() ;
+        activityNewNoteBinding.deadLineDateEditText.setText(sdf.format(date));
+
+    }
+
 
 }
